4267229bb08491c96ec2cc4f8345e6c2befc46ed
DaniyalAsif7778 <daniyal.personel7778@gmail.com>
2026-05-13 19:37:57 +0500

    learn data modeling & practice ecommerce app data modeling

--- models/ecommerce/users.models.js
+++ models/ecommerce/users.models.js
@@ -2,9 +2,34 @@ import mongoose from "mongoose"
 
   const userSchema = new  mongoose.Schema({
 
+   userName:{
+    type:String,
+    unique:true,
+    lowerCase:true,
+    required:true,
 
 
-},{timeStamps:true})
+   },
+
+   email:{
+    type:String,
+    unique:true,
+    lowerCase:true,
+    required:true,
+
+
+   },
+
+    password:{
+    type:String,
+    unique:true,
+    lowerCase:true,
+    required:true,
+
+
+   }
+
+},{timestamps:true})
 
 
 
