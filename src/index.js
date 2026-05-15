75d7bfaf6804b8a997d4ac7308707f531499e258
DaniyalAsif7778 <daniyal.personel7778@gmail.com>
2026-05-15 14:33:55 +0500

    Learn how to connect backend with mongodb database & practice two way to connect with mongodb database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
+// import express from 'express';
+
+// const app = express();
+
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('hellow dearn server is listening');
+// });
+
+// app.listen(PORT, (req, res) => {
+//   console.log('app is listing at the 3000');
+// });
+
+import mongoose from 'mongoose';
 import express from 'express';
+import dotenv from 'dotenv';
+import { DB_NAME } from './constant.js';
 
 const app = express();
 
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('hellow dearn server is listening');
+dotenv.config({
+    path: './env',
 });
 
-app.listen(PORT, (req, res) => {
-  console.log('app is listing at the 3000');
-});
+(async () => {
+    try {
+        const data = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`,
+        );
+        console.log(
+            `MongoDB connected || DB HOST : ${data.connections[0].host}`,
+        );
+        app.on('error', (error) => {
+            console.log('error', error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, (req, res) => {
+            console.log('app is listening at port 8000');
+        });
+    } catch (error) {
+        console.log('error', error);
+        throw error;
+    }
+})();
